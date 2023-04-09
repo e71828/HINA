b799bb17975e39911d03f8b867dc4823adb3a4aa
e71828 <740943170@qq.com>
2023-04-09 17:17:11 +0800

    add method of IFFT

--- pilot and example/test.m
+++ pilot and example/test.m
@@ -1,4 +1,4 @@
-%% 群延迟
+%% 1. 群延迟方法
 pilot = load("pilot.mat");
 example_64Tc = load("example_64Tc.mat");
 Xf = pilot.pilot;
@@ -24,3 +24,46 @@ srs_bw = num_srs_subcarriers * delta_f;  % SRS信号的带宽
 % 计算频率中心，估计值
 srs_subcarriers = floor((N - num_srs_subcarriers) / 2) + (1:num_srs_subcarriers);
 frequencies = fC + (srs_subcarriers - N/2) * delta_f;
+
+%% 2. 逆Fourier方法
+% generate x with Yf and Xf.
+x = Yf./Xf;
+X = ifft(x);
+
+% 计算频率轴
+Nx = length(x); % 信号长度
+fs = 1/srs_spacing;
+f = (0:Nx-1)*(fs/Nx); % 频率轴
+
+% 绘制复数信号的频谱
+figure;
+plot(f/TC, abs(X));
+grid on;
+xlim([0 256])
+
+% 应该是 64 TC，但只能得到60 TC。
+
+%% 复数信号案例-测试时延分辨率
+% 创建一个复数信号
+% TC = 1/(480 * 1000 * 4096); 
+
+fs = 1/srs_spacing; % 采样频率（Hz）
+t = (-num_srs_subcarriers/2:num_srs_subcarriers/2-1)/fs; % 时间向量
+f1 = 64*TC; % 信号频率1（Hz）
+f2 = 120*TC; % 信号频率2（Hz）
+x = exp(1i*2*pi*f1*t) + exp(1i*2*pi*f2*t); % 复数信号
+
+% 对复数信号进行傅里叶变换
+X = fft(x);
+
+% 计算频率轴
+Nx = length(x); % 信号长度
+f = (0:Nx-1)*(fs/Nx); % 频率轴
+
+% 绘制复数信号的频谱
+figure;
+plot(f/TC, abs(X));
+grid on;
+xlim([0 256])
+
+% 时延分辨率低为 20TC。
